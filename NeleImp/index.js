01823eec33c435a5b931fa4bc84dd20987cc35e2
Imp-nele <nele2ck@gmail.com>
2024-10-25 11:40:41 +0200

    change

--- NeleImp/index.js
+++ NeleImp/index.js
@@ -1,22 +1,5 @@
-const obj = {
-  a: 1,
-};
-//au debut on cree un objet vide et js y associe des proprietes par defaut
-console.log(Object.getPrototypeOf(obj)); // getPrototypeof = static fn
-console.log(Object.getPrototypeOf(2)); //Prototype String =>
-
-class Person {
-  age;
-  prenom;
-  nom;
-  constructor(prenom, nom) {
-    this.prenom = prenom;
-    this.nom = nom;
-  }
-  static sayHello() {
-    return "Hello";
-  }
-  // pour l'appeler je fais Person.sayHello parce que c'est static
+document.getElementById("btn").addEventListener("click", changeColor()); // addEventListener : Eventloop
+function changeColor() {
+  const square = document.getElementById("square");
+  square.style.backgroundColor = "red";
 }
-b = new Person("Nele", "IMP");
-console.log(b);
