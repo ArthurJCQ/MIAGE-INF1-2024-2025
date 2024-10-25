f5101a6708c583c8a4092f646d29629f1d317a6a
douaafr <douaafr19@gmail.com>
2024-10-25 15:00:13 +0200

    séance 25/10

---
+++ DouaaF/index.js
@@ -0,0 +1,75 @@
+// const obj = {
+//     a: 1
+// };
+
+// console.log(obj);
+// console.log(Object.getPrototypeOf(obj));
+// console.log(Object.getPrototypeOf('chaine'));
+// console.log(typeof String); //Les prototypes sont les fonctions
+
+
+// class Person{
+//      //constructeur avec un prénom et un nom
+//      //une popriété age
+//      //on crée une nouvelle personne et on accède à nom et prénom etc..
+
+//     age = 20;
+//     _city = '';
+
+//      constructor (prenom, nom){
+//         this.prenom = prenom;
+//         this.nom = nom;
+//      }
+
+//      incrementAge(){
+//         this.age++;
+//      }
+
+//      set city(v){
+//         this._city = v;
+//      }
+
+//      get nom(){
+//         return this._nom;
+//      }
+
+//      get prenom(){
+//         return this._prenom;
+//      }
+
+//      set nom(nom){
+//         this._nom = nom;
+//      }
+    
+//      set prenom(prenom){
+//         this._prenom = prenom;
+//      }
+
+//      isMajeur(){
+//         return this.age >= 18;
+//      }
+
+//     static majorite = 18; 
+
+//     static sayHello(){
+//         console.log('Hello !');
+//      }
+
+// }
+
+// const John = new Person('John', 'Doe');
+// console.log(John);
+
+
+function randomColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
+}
+document.getElementById("btn").addEventListener('click', () => {
+    const square = document.getElementById('square');
+    square.style.backgroundColor = `#${randomColor()}`;
+})
+
+document.getElementById("btn2").addEventListener('click', () => {
+    //désactiver le premier bouton si on appuie une premiere fois et l'activer si on appuie une deuxieme fois
+    document.getElementById("btn").disabled = !document.getElementById("btn").disabled;
+})
