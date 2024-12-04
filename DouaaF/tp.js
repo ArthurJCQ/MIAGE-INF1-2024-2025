ea3e77c31b43f1459646b1eb01505480ba89e503
douaafr <douaafr19@gmail.com>
2024-12-04 14:25:23 +0100

    ajout coeff

---
+++ DouaaF/tp.js
@@ -0,0 +1,12 @@
+const notesInf1 = [3, 13, 18, 15, 10, 3];
+
+const notesAvecCoefficient = notesInf1.map(note => {
+  return {
+    note: note,
+    coefficient: note < 10 ? 1.5 : 3
+  };
+});
+
+console.log(notesAvecCoefficient);
+
+
