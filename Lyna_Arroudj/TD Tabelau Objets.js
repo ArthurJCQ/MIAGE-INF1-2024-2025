443c11bb8538eba66988b2841c386018171c4c87
lynaarroudj <lynaarroudj@gmail.com>
2024-12-04 14:44:55 +0100

    Tableau Coeff

---
+++ Lyna_Arroudj/TD Tabelau Objets.js
@@ -0,0 +1,12 @@
+
+const notes = [8, 12, 9, 15, 7, 10, 13];
+
+const notesCoefficient = notes.map(note => {
+    return {
+        note: note,
+        coefficient: note < 10 ? 1.5 : 3
+    };
+});
+
+
+console.log(notesCoefficient);
