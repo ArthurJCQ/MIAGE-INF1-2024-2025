5cc87eeb20e71054735b52a320058ec2082ec290
cleme <clement.sauvonnet@gmail.com>
2024-12-04 14:51:14 +0100

    ajout somme des carrés du tableau

--- clement_sauvonnet/tableaux.js
+++ clement_sauvonnet/tableaux.js
@@ -1,4 +1,18 @@
 const table = [1,2,12,3,4,10];
 
-const mapTableau = table.map(note => note > 10 ? {valeur : note, "coefficient" : 1.5} : {valeur : note, "coefficient" : 3});
-console.log(mapTableau);
+const tableModifiee = table.map(note => note > 10 ? {valeur : note, coefficient : 1.5} : {valeur : note, coefficient : 3});
+console.log(tableModifiee);
+
+const table2 = table.map(note => {
+    return {
+        valeur: note,
+        coefficient: note > 10 ? 1.5 : 3
+    }
+});
+const sumOfSquaresOfEvens = (numbers) =>
+    numbers
+        .filter((num) => num % 2 === 0)
+        .map((num) => num * num)
+        .reduce ((stack, sum) => sum + stack, 0);
+
+console.log(sumOfSquaresOfEvens(table));
