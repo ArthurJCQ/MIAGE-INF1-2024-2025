3cce64b20537ade45e8ba15c096819fbf5cfd85c
lisadahoumane <148991818+lisadahoumane@users.noreply.github.com>
2024-12-04 16:20:56 +0100

    Update fetchCatFact.js

--- Liza/fetchCatFact.js
+++ Liza/fetchCatFact.js
@@ -1,11 +1,10 @@
-// Fonction pour récupérer un fait sur les chats
 const fetchCatFact = async () => {
   const response = await fetch('https://catfact.ninja/fact');
   const result = await response.json();
   return result; // Renvoie les données JSON
 };
 
-// Fonction pour afficher un fait sur les chats
+// Fonction pour affichage
 async function displayFact() {
   const catFact = await fetchCatFact(); // Appelle la fonction précédente
   console.log(catFact); // Affiche l'objet complet
