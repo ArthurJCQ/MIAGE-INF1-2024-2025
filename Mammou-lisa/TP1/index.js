c0c65dae763ca726284ee5d21571516c62c9f99a
lisaamm <126359205+lisaamm@users.noreply.github.com>
2024-12-09 23:19:14 +0100

    index.js

---
+++ Mammou-lisa/TP1/index.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", changeColor()); 
+function changeColor() {
+  const square = document.getElementById("square");
+  square.style.backgroundColor = "red";
+}
