8fa0ebf6303d061a6fec8446da11b0b900f850b6
lisaamm <126359205+lisaamm@users.noreply.github.com>
2024-12-10 12:03:43 +0100

    Update index.js

--- Mammou-lisa/TP1/index.js
+++ Mammou-lisa/TP1/index.js
@@ -1,5 +1,38 @@
-document.getElementById("btn").addEventListener("click", changeColor()); 
-function changeColor() {
-  const square = document.getElementById("square");
-  square.style.backgroundColor = "red";
+
+const square1 = document.getElementById("square1");
+const square2 = document.getElementById("square2");
+const clickButton = document.getElementById("click-btn");
+const resetButton = document.getElementById("reset-btn");
+
+
+const originalColor = "pink";
+let selectedSquare = square1;
+
+
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+function selectSquare(square) {
+ 
+  square1.classList.remove("selected");
+  square2.classList.remove("selected");
+
+ 
+  square.classList.add("selected");
+
+ 
+  selectedSquare = square;
+}
+square1.addEventListener("click", () => selectSquare(square1));
+square2.addEventListener("click", () => selectSquare(square2));
+
+clickButton.addEventListener("click", () => {
+  selectedSquare.style.backgroundColor = getRandomColor();
+});
+
+
+resetButton.addEventListener("click", () => {
+ 
+  square1.style.backgroundColor = originalColor;
+  square2.style.backgroundColor = originalColor;
+});
